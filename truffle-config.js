e7ee609ec7005571e396e71923aa06c51c129797
Anthony Cho <tony3918@hotmail.com>
2019-08-09 23:09:01 +0900

    revert

--- truffle-config.js
+++ truffle-config.js
@@ -1,17 +1,9 @@
 module.exports = {
   networks: {
-    // development: {
-    //   host: "127.0.0.1", // Localhost (default: none)
-    //   port: 22000, // Standard Ethereum port (default: none)
-    //   network_id: "*", // Any network (default: none)
-    //   gasPrice: 0,
-    //   gas: 4500000,
-    //   type: "quorum"
-    // },
     development: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*",
+      host: "127.0.0.1", // Localhost (default: none)
+      port: 22000, // Standard Ethereum port (default: none)
+      network_id: "*", // Any network (default: none)
       gasPrice: 0,
       gas: 4500000,
       type: "quorum"
